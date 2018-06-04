65c04848c2f09587e488a7e5ab779aa1823ca52a
Abe Queen <abedababe8@gmail.com>
2018-06-04 09:49:43 -0700

    file system setup

---
+++ db/migrations/20180604094411_01_users.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments();
+    table.string('first_name').notNullable().defaultsTo('');
+    table.string('last_name').notNullable().defaultsTo('');
+    table.string('email').notNullable().defaultsTo('');
+    table.string('hashed_password').notNullable().defaultsTo('');
+    table.boolean('admin').notNullable().defaultsTo(false)
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users')
+};
