b6f9420c35f20a63cd0f13fd40c3c21194b1586a
Abe Queen <abedababe8@gmail.com>
2018-06-04 11:41:06 -0700

    migration rename

--- db/migrations/20180604105821_votes.js
+++ db/migrations/20180604105821_votes.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('votes', (table) => {
     table.increments();
     table.string('user_id').notNullable().defaultsTo('').references('users.id');
-    table.string('comment_id').notNullable().defaultsTo('').references('cities.id');
+    table.string('comment_id').notNullable().defaultsTo('').references('comments.id');
     table.enu('vote', (-1, 1))
     table.timestamps(true, true)
   });
