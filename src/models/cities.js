2cf25e06dee93efcf7fcb24f7dcd095d6305534e
Abe Queen <abedababe8@gmail.com>
2018-06-07 13:17:04 -0700

    appearantly made changes

--- src/models/cities.js
+++ src/models/cities.js
@@ -8,8 +8,8 @@ const bcrypt = require("bcrypt-as-promised");
 function getAllWithMostComments() {
     console.log('made it to model')
   return knex.raw(`
-        select comments.city_id as id, name, img, count(*) as comment_count from 
-        comments join cities on cities.id = comments.city_id  group by 
+        select comments.city_id as id, name, img, count(*) as comment_count from
+        comments join cities on cities.id = comments.city_id  group by
         comments.city_id, cities.name, cities.img order by comment_count desc limit 3;
         `)
         .then(data => {
